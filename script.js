602a527c0062c8134e8b1dd65f517dfb300f0cb5
Yash Patil <ya88287946@gmail.com>
2026-02-08 17:20:27 +0530

    Set up HTTP server

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const  myhttp = require('http');
+const routes = require("./routes");
+
+routes.testFun();
+const myServer = myhttp.createServer(routes.handler);
+
+myServer.listen(3000,()=>{
+  console.log('Server is running at port 3000');  
+})
